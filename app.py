193e93ac56bc23f0eeafb07e2a5d3f1c97e0f5b1
tonsh <tonshlee@gmail.com>
2016-06-02 19:02:02 +0800

    Add yaml config

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 # coding=utf-8
 
-import os
-
+import yaml
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-if os.environ.get('STOCK_IS_PRO'):
-    app.config.from_object('config.product.ProductConfig')
-else:
-    app.config.from_object('config.DevelopmentConfig')
+
+app_config = yaml.load(file(app.root_path + '/app.yaml', 'r'))
+
+app.DEBUG = app_config['debug']
+app.config.update({
+    'SQLALCHEMY_DATABASE_URI': 'mysql+pymysql://%s:%s@%s:%s/%s' % (
+        app_config['mysql']['user'], app_config['mysql']['pwd'],
+        app_config['mysql']['host'], app_config['mysql']['port'],
+        app_config['mysql']['db']),
+    'SQLALCHEMY_TRACK_MODIFICATIONS': True,
+})
 
 db = SQLAlchemy(app)
